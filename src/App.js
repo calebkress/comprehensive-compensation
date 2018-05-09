690c3fe156e6c289bab5ea5295e04214875b5ab4
Caleb Kress <calebkress@gmail.com>
2018-05-09 10:54:11 -0500

    changed label padding, added todo in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import PtoSlider from './sliders/PtoSlider';
 import EducationSlider from './sliders/EducationSlider';
 import './App.css';
 
+// TODO: Move the repeated handleOnChange logic from each individual component
+// up to the App.js level - keeps code DRY and allows value storage in global
+// state. 
+
+
 class App extends Component {
   render() {
     return (
